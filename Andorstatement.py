f42a3da060a92cfb9eceb7803df1c7671ee3ad52
Tani-alt <Tanirika.gupta@gmail.com>
2025-01-13 21:27:09 +0530

    Elif statement

---
+++ Andorstatement.py
@@ -0,0 +1,16 @@
+#ifelifandelse
+To=934
+Me=78
+You=-256
+if To > 0 and  Me > 0 and You > 0 :
+   print("all numbers are positive")
+else:
+   print("all number are not positive")
+
+To=934
+Me=78
+You=-256
+if To > 0 or Me > 0 or You > 0 :
+   print("all numbers are positive")
+else:
+   print("all number are not positive")
