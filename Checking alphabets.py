b64be08241a8e06ea81058531455df9f01aa1256
Tani-alt <Tanirika.gupta@gmail.com>
2025-01-18 10:15:24 +0530

    Checking alphabets

---
+++ Checking alphabets.py
@@ -0,0 +1,15 @@
+#Checkingalphabets
+2=="B"
+67=="D"
+8=="G"
+if 2 == "A" :
+    print(2,"comes before A")
+
+else :
+    print(2,"comes after  A")
+
+if 67 == "C" :
+    print(67,"comes first from C")
+
+else :
+    print(67,"comes after  C")
