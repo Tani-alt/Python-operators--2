2677bd8ba694c5f7d53e4f143236245072c7eaf4
Tani-alt <Tanirika.gupta@gmail.com>
2025-01-27 19:37:46 +0530

    Project

---
+++ Classproject.py
@@ -0,0 +1,19 @@
+#Checking alphabets
+A=2.75
+B="k"
+C=-73
+if A== 2.75 :
+    print(A,"is an float")
+else :
+    print(A,"is a alphabet")
+
+if B== "k" :
+    print(B,"is an alphabet")
+else :
+    print(B,"is an integer")
+
+if C== -73 : 
+    print(C,"is an integer")
+else :
+    print(C,"is an alphabet")
+
